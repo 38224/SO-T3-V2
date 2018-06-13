88a13c69dd944c0725527168a459a2aeeeef312e
38224 <38224@alunos.isel.ipl.pt>
2018-06-13 22:59:13 +0100

    ololol

--- TestFolderCopy/TestFolderCopy.cpp
+++ TestFolderCopy/TestFolderCopy.cpp
@@ -3,6 +3,13 @@
 
 #include "stdafx.h"
 #include "../AsyncFileCopy/AsyncFileCopy.h"
+#include <windows.h>
+#include <tchar.h> 
+#include <stdio.h>
+#include <strsafe.h>
+#pragma comment(lib, "User32.lib")
+
+
 
 #define DllImport   __declspec( dllimport )  
 
@@ -17,23 +24,46 @@ VOID mycb(LPVOID userCtx, DWORD status, UINT64 transferedBytes)
 
 int main()
 {
-
 	LPCTSTR source = TEXT("inFile.txt"); // SO - T3 - V2\TestAsyncFileCopy
 	LPCTSTR destination = TEXT("outFile.txt");
+	WIN32_FIND_DATA ffd;
+	LARGE_INTEGER filesize;
+	TCHAR szDir[MAX_PATH];
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+	size_t length_of_arg;
 
 
+	StringCchLength((STRSAFE_LPCWSTR)"E:\\PROJ", MAX_PATH, &length_of_arg);
+	StringCchCopy(szDir, MAX_PATH, (STRSAFE_LPCWSTR)"E:\\PROJ");
+	StringCchCat(szDir, MAX_PATH, TEXT("\\*"));
+
+	hFind = FindFirstFile(szDir, &ffd);
+
+	if (INVALID_HANDLE_VALUE == hFind)
+	{
+		printf("invalid hFind handle");
+		getchar();
+		exit(1);
+	}
 
 
+	do
+	{
+		if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+		{
+			_tprintf(TEXT("  %s   <DIR>\n"), ffd.cFileName);
+		}
+		else
+		{
+			filesize.LowPart = ffd.nFileSizeLow;
+			filesize.HighPart = ffd.nFileSizeHigh;
+			_tprintf(TEXT("  %s   %ld bytes\n"), ffd.cFileName, filesize.QuadPart);
+			getchar();
+		}
+	} while (FindNextFile(hFind, &ffd) != 0);
 
 
-
-
-
-
-
-
-
-
+	 
 
 	CONTEXT myctx;
 	GetThreadContext(GetCurrentThread(), &myctx);
